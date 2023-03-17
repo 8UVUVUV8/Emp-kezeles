c06140589d3b832deef6a3c31427742b7e48b3ff
8UVUVUV8 <sz.andras2400@gmail.com>
2023-03-17 10:48:11 +0100

    jövöhét folyt köv

--- src/app.js
+++ src/app.js
@@ -1,36 +1,84 @@
 const doc = {
-    tbody: null
+    tbody: null,
+    saveB : null
+
 };
 const state = {
-    dolgozoLista: []
+    dolgozoLista: [],
+    host: null
 };
 
 window.addEventListener('load', () => {
     init();
-    render();
+    
+    
 })
 
 function init() {
+    state.host = 'http://localhost:8000/'
     doc.tbody = document.querySelector('#tbody');
-    state.dolgozoLista = [
-        { id: 1,  name: 'Pisti', city: 'Szolnok', salary: 385 },
-        { id: 2, name: 'Mari', city: 'Szeged', salary: 382 },
-        { id: 3, name: 'Kati', city: 'Szolnok', salary: 355 },
-        { id: 4, name: 'Dani', city: 'Szeged', salary: 384 },
-        { id: 5, name: 'Lili', city: 'Szolnok', salary: 382 }
-    ];
+    doc.saveB = document.querySelector('#savebutton');
+    doc.saveB.addEventListener('clcik', () => {
+        console.log("mukszik")
+    });
+    getEmp()    
 }
+
+function getEmp(){
+    let point = 'employees';
+    let url = state.host + point;
+    fetch(url)
+    .then( response => {
+        return response.json()
+    } )
+    .then(result => {
+        console.log(result);
+        state.dolgozoLista = result
+        render();
+    });
+
+}
+
+function deleteEmployee(id) {
+    let point = 'employees';
+    let url = state.host + point + '/' + id
+    fetch(url, {
+        method: 'delete'
+    })
+    .then(res => res.json())
+    .then(res =>{
+        console.log(res);
+        getEmp();
+        render();
+    })
+
+
+}
+
+function startdeleteEmployee(event){
+    let id = (event.getAttribute('data-id'));
+    deleteEmployee(id)
+}
+
+
+
 
 function render() {
     let rows = '';
     state.dolgozoLista.forEach( dolgozo => {
-        console.log(dolgozo.name)
+        
         rows += `
             <tr>
                 <td>${dolgozo.id}</td>
                 <td>${dolgozo.name}</td>
                 <td>${dolgozo.city}</td>
                 <td>${dolgozo.salary}</td>
+                <td>
+                    <button classs="btn btn-warning"
+                    onclick = "startdeleteEmployee(this)"
+                    data-id = "${dolgozo.id}"
+                    >Törlés</button>
+                </td>
             </tr>
         `;
 
